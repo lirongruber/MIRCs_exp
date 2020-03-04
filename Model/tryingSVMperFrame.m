e69b96b9b760ae408a333a76fd021bcd73efd212
lirongruber <35901248+lirongruber@users.noreply.github.com>
2020-03-04 18:06:33 +0200

    compliting SVM

--- Model/tryingSVMperFrame.m
+++ Model/tryingSVMperFrame.m
@@ -9,7 +9,7 @@ folders={'Recognized','Not Recognized'};
 movies_path='C:\Users\lirongr\Documents\MIRCs_exp\data\modelData\videos';
 folders={'MIRCs_yes','MIRCs_no','subMIRCs_yes','subMIRCs_no'};
 
-for FixationNumToUse=1:7
+for FixationNumToUse=1:5
     for repetition=1:2
         clearvars -except movies_path folders FixationNumToUse repetition perCorrect_final perCorrect_l perCorrect_g perCorrect_f
         forSVM={};
@@ -20,24 +20,23 @@ for FixationNumToUse=1:7
             rel=1;
             functions=nan(1,1);
             for currMov = movies'
-                if ~strcmp(currMov.name,'.') && ~strcmp(currMov.name,'..') && strcmp(currMov.name(end-4),'r')
-%                     load([movies_path '\' folder{1,1} '\' currMov.name], 'filt_movie');
-                    load([movies_path '\' folder{1,1} '\' currMov.name], 'rel_movie_notCorr'); filt_movie=rel_movie_notCorr;
+                if ~strcmp(currMov.name,'.') && ~strcmp(currMov.name,'..') && ~strcmp(currMov.name(end-4),'r')
+                    load([movies_path '\' folder{1,1} '\' currMov.name], 'filt_movie');
                     relFixation=min(FixationNumToUse,size(filt_movie,2));
                     currT=filt_movie{1,relFixation}(:,:,2);
-                    if size(currT(currT~=0)',2)>100
-                        functions(rel,1:size(currT(currT~=0)',2))=currT(currT~=0)';
-                        rel=rel+1
-                    end
+                    
+                    functions(rel,1:size(currT(currT~=0)',2))=currT(currT~=0)';
+                    rel=rel+1;
+                    disp(['trial =' num2str(rel)])
                 end
             end
             forSVM{folder_num}=functions;
         end
-        forSVM={ forSVM{1,:} forSVM{3,:} ; forSVM{2,:}  forSVM{4,:} };
+        forSVM={ [forSVM{1,1} ; forSVM{1,3}] , [forSVM{1,2} ; forSVM{1,4} ]};
         forSVM{1}=Shuffle(forSVM{1},2);
         forSVM{2}=Shuffle(forSVM{2},2);
         
-        l=min(size(forSVM{1}(:,5:end),2),size(forSVM{2}(:,5:end),2));% after 50 ms - cutting the begining for class
+        l=min(size(forSVM{1}(:,1:end),2),size(forSVM{2}(:,1:end),2));
         trialPerGroup=min(size(forSVM{1},1),size(forSVM{2},1));% making sure same group sizes
         X=[forSVM{1}(1:trialPerGroup,end-l+1:end) ; forSVM{2}(1:trialPerGroup,end-l+1:end)];
         
@@ -55,6 +54,7 @@ for FixationNumToUse=1:7
         idx_1=1:size(XX,1)/2;
         idx_0=size(XX,1)/2+1:size(XX,1);
         for i=1:idx_1(end)
+            disp(num2str(i))
             totalNlabels=totalNlabels+1;
             leaveOut=setdiff(1:size(XX,1),[idx_1(i) idx_0(i)]);
             X_train=XX(leaveOut,:);
@@ -71,7 +71,6 @@ for FixationNumToUse=1:7
             [label_f(totalNlabels,:),score_f] = predict(SVMModel_f,x_test);
             [label_g(totalNlabels,:),score_g] = predict(SVMModel_g,x_test);
             [label_l(totalNlabels,:),score_l] = predict(SVMModel_l,x_test);
-            
             
         end
         label_final=zeros(size(label_f));
